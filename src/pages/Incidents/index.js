99ea75f87b38054d96c559397179796685cc9f59
lucazweb <lucazwebmail@gmail.com>
2020-03-28 11:48:55 -0300

    Mobile application basic structure with React Native
    - Add Routes component
    - Add Pages Incident and Details with own styles
    - Create stack navigation with react navigation package
    - Change splash screen

---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+import logoImg from "../../assets/logo.png";
+import styles from "./styles";
+
+const Incidents = () => (
+  <View style={styles.container}>
+    <View style={styles.header}>
+      <Image source={logoImg} />
+      <Text style={styles.headerText}>
+        Total de <Text style={styles.headerTextBold}>0 casos</Text>
+      </Text>
+    </View>
+    <Text style={styles.title}>Bem vindo!</Text>
+    <Text style={styles.description}>
+      Escolha um dos casos abaixo e salve o dia.
+    </Text>
+    <FlatList 
+      data={[0, 1, 2]}
+      style={styles.incidentsList} 
+      keuExtractor={incident => String(incident)}
+      showsVerticalScrollIndicator={false}
+      renderItem={ () => (
+        <View key={Math.random()} style={styles.incident}>
+          <Text style={styles.incidenteProperty}>ONG:</Text> 
+          <Text style={styles.incidentValue}>APAD:</Text>
+
+          <Text style={styles.incidenteProperty}>CASO:</Text>
+          <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+          <Text style={styles.incidenteProperty}>VALOR:</Text>
+          <Text style={styles.incidentValue}>R$ 120,00</Text>
+          <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+            <Feather name="arrow-right" size={16} color="#E02041" />
+          </TouchableOpacity>          
+        </View>
+      )}
+      >
+
+    </FlatList>
+  </View>
+);
+
+export default Incidents;
